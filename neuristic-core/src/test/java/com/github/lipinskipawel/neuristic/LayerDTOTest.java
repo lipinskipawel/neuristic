82249eb1ec832c45380a60b877a781370c6f917c
Pawel Lipinski <plipinski@atlassian.com>
2020-06-08 13:41:36 +0200

    Clean up unit test - LayerDTOTest

--- neuristic-core/src/test/java/com/github/lipinskipawel/neuristic/LayerDTOTest.java
+++ neuristic-core/src/test/java/com/github/lipinskipawel/neuristic/LayerDTOTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -46,16 +48,20 @@ class LayerDTOTest {
         @Test
         @DisplayName("parse 1d arrays")
         void shouldParseStringFrom1dArray() {
-            final var preparedWeight = Matrix.of(new double[][]{{0.6775818874783545, 0.39148446642093626, 0.5835065076748652, 0.2719096671665818, -0.32859816820175425}});
-            final var preparedBiases = Matrix.of(new double[][]{{0.08625705839435478}});
+            final var forWeights = new double[][]{{0.6775818874783545, 0.39148446642093626, 0.5835065076748652, 0.2719096671665818, -0.32859816820175425}};
+            final var forBiases = new double[][]{{0.08625705839435478}};
+            final var preparedWeight = Matrix.of(forWeights);
+            final var preparedBiases = Matrix.of(forBiases);
+
             final var layerDTO = LayerDTO.parseStringDoubleWeightDoubleBiasClassActivationFunction(
-                    "[[0.6775818874783545, 0.39148446642093626, 0.5835065076748652, 0.2719096671665818, -0.32859816820175425]]:" +
-                            "[[0.08625705839435478]]:" + Sigmoid.class.getCanonicalName());
+                    Arrays.deepToString(forWeights) + ":" +
+                            Arrays.deepToString(forBiases) + ":" +
+                            Sigmoid.class.getCanonicalName()
+            );
 
             final var weight = layerDTO.getWeight();
             final var biases = layerDTO.getBiases();
             final var activationFunction = layerDTO.getActivationFunction();
-
             assertAll(
                     () -> Assertions.assertThat(weight).usingRecursiveComparison().isEqualTo(preparedWeight),
                     () -> Assertions.assertThat(biases).usingRecursiveComparison().isEqualTo(preparedBiases),
@@ -66,16 +72,20 @@ class LayerDTOTest {
         @Test
         @DisplayName("parse 2d arrays")
         void shouldParseStringFrom2dArray() {
-            final var preparedWeight = Matrix.of(new double[][]{{0.8, -1.2}, {-4.2, 3}});
-            final var preparedBiases = Matrix.of(new double[][]{{1, 1}, {2, -0.9}});
-            final var layerDTO = LayerDTO
-                    .parseStringDoubleWeightDoubleBiasClassActivationFunction("[[0.8, -1.2], [-4.2, 3]]:" +
-                            "[[1, 1], [2, -0.9]]:" + Sigmoid.class.getCanonicalName());
+            final var forWeights = new double[][]{{0.8, -1.2}, {-4.2, 3}};
+            final var forBiases = new double[][]{{1, 1}, {2, -0.9}};
+            final var preparedWeight = Matrix.of(forWeights);
+            final var preparedBiases = Matrix.of(forBiases);
+
+            final var layerDTO = LayerDTO.parseStringDoubleWeightDoubleBiasClassActivationFunction(
+                    Arrays.deepToString(forWeights) + ":" +
+                            Arrays.deepToString(forBiases) + ":" +
+                            Sigmoid.class.getCanonicalName()
+            );
 
             final var weight = layerDTO.getWeight();
             final var biases = layerDTO.getBiases();
             final var activationFunction = layerDTO.getActivationFunction();
-
             assertAll(
                     () -> Assertions.assertThat(weight).usingRecursiveComparison().isEqualTo(preparedWeight),
                     () -> Assertions.assertThat(biases).usingRecursiveComparison().isEqualTo(preparedBiases),
